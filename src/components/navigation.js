e6b288fca3a54664555c7045d752ea650567dbc6
Craig <craig.skicko@gmail.com>
2022-10-07 17:11:40 +1100

    Added functionality to update the activePage state on nav clicks

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 export default function Navigation({ activePage, setActivePage }) {
+    // Handle navigation based on clicked navigation button
+    const navigate = (e) => {
+        e.preventDefault();
+        setActivePage(e.target.innerHTML);
+    }
 
     return (
         <div className="nav-container">
             <div className="navbar-nav">
-                <a className="nav-link" href="#">About Me</a>
-                <a className="nav-link" href="#">Portfolio</a>
-                <a className="nav-link" href="#">Contact</a>
-                <a className="nav-link" href="#">Resume</a>
+                <a className="nav-link" href ='About Me' onClick={navigate}>About Me</a>
+                <a className="nav-link" href ='Portfolio' onClick={navigate}>Portfolio</a>
+                <a className="nav-link" href ='Contact' onClick={navigate}>Contact</a>
+                <a className="nav-link" href ='Resume' onClick={navigate}>Resume</a>
                 <p>Active page is {activePage}</p>
             </div>
         </div>
