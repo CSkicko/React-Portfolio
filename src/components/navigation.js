75c8c894e294aed15ebdafd648d6dd6a9fab96e9
Craig <craig.skicko@gmail.com>
2022-10-07 16:26:43 +1100

    Added components files and updated imports

---
+++ src/components/navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navigation() {
+    return (
+        <div className="nav-container">
+            <h1>Navigation</h1>
+        </div>
+    );
+}
