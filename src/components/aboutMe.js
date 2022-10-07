7a3e0a3e7034dcf304dd257b33eb7d81d213e5d3
Craig <craig.skicko@gmail.com>
2022-10-07 19:47:24 +1100

    Basic code added to about me page - styling yet to be produced

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/about.css';
 
 export default function AboutMe() {
     return (
         <div className="about-container">
-            <h1>About Me</h1>
+            <h2>About Me</h2>
+            <div id="profile" role="img" aria-label="Image of Craig"></div>
+            <p>I have been working as an Engineering Consultant for over 7 years, delivering a variety of projects across the UK and Australia.</p>
+            <p>I have always had an interest in solving analytical problems, and from a young age showed talent as a programmer.</p>
+            <p>Recently, I discovered a passion for full stack web development and set out to build some simple applications using a variety of languages, tools and frameworks, including HTML, CSS, Javascript, Postgres and Django</p>
+            <p>To further hone my skills, I have completed the Trilogy Web Development Bootcamp, run by Monash University. Here I have progressed my understanding of web development and broadened my knowledge of programming languages, computer science and industry best practices</p>
+            <p>Moving forward, I would welcome discussions surrounding opportunities to further utilise my skillset and deliver full stack applications to industry clients.</p>
         </div>
     );
 }
