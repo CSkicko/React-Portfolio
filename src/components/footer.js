427d0a5ca9556ca6e7c8e541763e6c780ea75afe
Craig <craig.skicko@gmail.com>
2022-10-07 17:57:05 +1100

    Added icons and links in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 export default function Footer() {
     return (
         <div className="footer-container">
-            <h1>Footer</h1>
+            <a href='https://github.com/CSkicko' target='_blank' rel="noreferrer">
+                <FaGithub />
+            </a>
+            <a href='https://www.linkedin.com/in/craig-skicko-153b9184/' target='_blank' rel="noreferrer">
+                <FaLinkedin />
+            </a>
+            <a href='https://twitter.com/Craig_Skicko' target='_blank' rel="noreferrer">
+                <FaTwitterSquare />
+            </a>
         </div>
     );
 }
